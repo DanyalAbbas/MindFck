8012c972fe3f5c89ae5c56a77b4665a3205bd126
Danyal Abbas <abasdaniel61@gmail.com>
2024-12-22 17:41:08 +0500

    Copied app.py to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,7 @@ RUN pip install -r requirements.txt
 
 COPY ./ML_Models ./ML_Models
 COPY ./templates ./templates
+COPY ./app.py .
 
-CMD [ "python3",  "/app.py"]
+
+CMD [ "python3",  "./app.py"]
--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from sklearn import datasets
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
-import base64
-import io
 
 
 app = Flask(__name__)
@@ -17,65 +15,21 @@ model = None
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    global data
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and file.filename.endswith('.xlsx'):
-            data = pd.read_excel(file)
-            return redirect(url_for('choose_model'))
+    # global data
+    # if request.method == 'POST':
+    #     file = request.files['file']
+    #     if file and file.filename.endswith('.xlsx'):
+    #         data = pd.read_excel(file)
+    #         return redirect(url_for('choose_model'))
+# @app.route('/upload')
+# def upload_file():
+    
 
 @app.route('/choose_model')
 def choose_model():
     return render_template('choose_model.html')
 
-@app.route('/train_model', methods=['POST'])
-def train_model():
-    global model, data
-    if request.method == 'POST':
-        model_type = request.form['model']
-        X = [2,4,6,4,3,4,7,5]  # Adjust according to data
-        y = [2,2,6,8,3,1,6,8]       # Adjust according to data
-
-        if model_type == 'LinearRegression':
-            model = LinearRegression()
-            model.fit(X, y)
-
-        return redirect(url_for('plot_results'))
-
-@app.route('/plot_results')
-def plot_results():
-    # Testing and seeing how the model even shows data on the website
-    diabetes = datasets.load_diabetes()
-    diabetes_X = diabetes.data[:,np.newaxis, 2]
-
-    diabetes_X_train = diabetes_X[:-30]
-    diabetes_X_test = diabetes_X[-30:]
-
-    diabetes_Y_train = diabetes.target[:-30]
-    diabetes_Y_test = diabetes.target[-30:]
-
-    model = LinearRegression()
-
-    model.fit(diabetes_X_train,diabetes_Y_train)
-
-    diabetes_y_predicted = model.predict(diabetes_X_test)
-
-    plt.scatter(diabetes_X_test,diabetes_Y_test)
-    plt.plot(diabetes_X_test,diabetes_y_predicted)
-    # plt.show()
-
-        # Save plot to string buffer and encode
-    img = io.BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
-    plot_url = base64.b64encode(img.getvalue()).decode()
-
-    return render_template('plot.html', plot_url=plot_url)
-
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000)
 
-    app.run(debug=True)
+    # app.run(debug=True)
